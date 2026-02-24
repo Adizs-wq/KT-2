9157bf25377d441e74ebf34a82e793f89dca8128
Adizs-wq <Andrey04Kras@yandex.ru>
2026-02-24 19:02:17 +0300

    Doc

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def main():
-    print("Hello from kt-2!")
+from fastapi import FastAPI
 
+app = FastAPI()
 
-if __name__ == "__main__":
-    main()
+@app.get("/")
+def root():
+    return {"status": "ok"}
